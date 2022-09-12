dc8270982f1b274ef7ead516fe204ea442c34ee5
raniaadouni <rania.adouni@esprit.tn>
2022-09-12 16:24:46 +0100

    running mongodb replica with 3 Percona container"

---
+++ scripts/init.js
@@ -0,0 +1,20 @@
+rs.status();
+db.createUser({user: 'admin', pwd: 'admin', roles: [ { role: 'root', db: 'admin' } ]});
+db.getSiblingDB("admin").createRole({ "role": "pbmAnyAction",
+      "privileges": [
+         { "resource": { "anyResource": true },
+           "actions": [ "anyAction" ]
+         }
+      ],
+      "roles": []
+   });
+   db.getSiblingDB("admin").createUser({user: "pbmuser",
+   "pwd": "secretpwd",
+   "roles" : [
+      { "db" : "admin", "role" : "readWrite", "collection": "" },
+      { "db" : "admin", "role" : "backup" },
+      { "db" : "admin", "role" : "clusterMonitor" },
+      { "db" : "admin", "role" : "restore" },
+      { "db" : "admin", "role" : "pbmAnyAction" }
+   ]
+});
---
+++ scripts/pbm-set.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p /backup
+#pbm config --file /tmp/pbm_config.yaml
+
+
